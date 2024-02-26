b8dd888893ba852299853125918acb10e629c128
blerim2 <blerimballiu19@gmail.com>
2024-02-26 14:28:08 +0100

    base.model

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 
-'''Class Basemodel that defines all common atributes/methods for other classes'''
+"""Class BaseModel that defines all common attributes/methods for other classes"""
 
 
-import uuid import uuid4
+from uuid import uuid4
 import datetime
 
 class BaseModel:
-    '''Class basemodel'''
+    """class Basemodel"""
 
     def __init__(self):
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-       return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.datetime.now()
