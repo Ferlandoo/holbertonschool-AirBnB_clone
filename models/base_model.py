8cd16f53d0c956fb7c307bc162f952e47f121be3
blerim2 <blerimballiu19@gmail.com>
2024-02-26 14:03:52 +0100

    base

--- models/base_model.py
+++ models/base_model.py
@@ -7,17 +7,14 @@ class BaseModel:
 
 
     def __init__(self):
-        """Initialize the instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return a string representation of the instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
 
     
