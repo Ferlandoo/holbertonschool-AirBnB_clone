126bb0919dba99c3a38980ba8a66dec99eac1fd6
blerim2 <blerimballiu19@gmail.com>
2024-02-27 10:22:06 +0100

    update-dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -34,16 +34,3 @@ class BaseModel:
         dict_copy["created_at"] = self.created_at.isoformat()
         dict_copy["updated_at"] = self.updated_at.isoformat()
         return dict_copy
-
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
